e2506ee4080822cc762932a2ab5a585e5fe824e8
ihwang <ihwang@Subins-MacBook-Pro.local>
2020-03-27 17:23:41 +0200

    Modify to be valid on coding standard in 42school

--- src/ft_check_d_quote.c
+++ src/ft_check_d_quote.c
@@ -6,7 +6,7 @@
 /*   By: ihwang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/09 14:33:31 by ihwang            #+#    #+#             */
-/*   Updated: 2020/03/09 16:10:55 by ihwang           ###   ########.fr       */
+/*   Updated: 2020/03/27 17:16:59 by ihwang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int			ft_check_d_quote(char *str)
 	while (str[++i])
 	{
 		if (ft_is_d_quote(str[i]))
-		{	
+		{
 			flag = 1;
 			if (bal % 2 == 0)
 				bal++;
